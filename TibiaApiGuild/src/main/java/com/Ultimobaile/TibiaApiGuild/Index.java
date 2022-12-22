525ff3d45b7a4137fba4df3307c894e437bb987a
Luccas Gomes Maciel <luccas-maciel@hotmail.com>
2022-12-22 06:29:31 -0300

    adicao do index

--- TibiaApiGuild/src/main/java/com/Ultimobaile/TibiaApiGuild/Index.java
+++ TibiaApiGuild/src/main/java/com/Ultimobaile/TibiaApiGuild/Index.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TibiaApiGuildApplication {
+public class Index {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TibiaApiGuildApplication.class, args);
+		SpringApplication.run(Index.class, args);
 	}
 
 }
