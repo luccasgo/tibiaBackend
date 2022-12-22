4a44725ad03b5305dad7e9a8d5120ec7a71edf87
Luccas Gomes Maciel <luccas-maciel@hotmail.com>
2022-12-22 12:20:08 -0300

    get

--- TibiaApiGuild/src/main/java/com/Ultimobaile/TibiaApiGuild/controller/GuildController.java
+++ TibiaApiGuild/src/main/java/com/Ultimobaile/TibiaApiGuild/controller/GuildController.java
@@ -7,23 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Ultimobaile.TibiaApiGuild.ApiConsume;
 import com.Ultimobaile.TibiaApiGuild.domains.Personagem;
 
 @Controller
-@RequestMapping ("personagem")
+@RequestMapping("personagem")
 public class GuildController {
-	
+
 	ApiConsume apiConsume = new ApiConsume();
 
 	@GetMapping("/checar")
 	@CrossOrigin
-	public ResponseEntity<String> readBombas(@RequestBody Personagem personagem) throws ParseException, IOException {
+	public ResponseEntity<String> readBombas(@PathVariable Personagem personagem) throws ParseException, IOException {
 		return ResponseEntity.ok(apiConsume.getChares(personagem));
 	}
 
-	
+
 }
